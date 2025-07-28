347e648bf26f1eeda82b4fcb4409fb8cf4d54e21
Hanane Ait bah <hait-bah@e2r13p11.1337.ma>
2025-07-28 10:20:28 +0100

    done ,maybe

--- finalc03/ex00/ft_strcmp.c
+++ finalc03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: hait-bah <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 11:16:59 by hait-bah          #+#    #+#             */
-/*   Updated: 2025/07/27 19:01:04 by hait-bah         ###   ########.fr       */
+/*   Updated: 2025/07/28 10:17:51 by hait-bah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] -(unsigned char) s2[i]);
 }
 /*
 int main ()
--- finalc03/ex01/ft_strncmp.c
+++ finalc03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hait-bah <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 12:22:40 by hait-bah          #+#    #+#             */
-/*   Updated: 2025/07/27 19:29:13 by hait-bah         ###   ########.fr       */
+/*   Updated: 2025/07/28 10:18:48 by hait-bah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		i++;
 	if (i == n)
 		return (0);
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 /*
 int	main(void)
